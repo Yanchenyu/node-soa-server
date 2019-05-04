013a781f57a12b3afd033cfead5c6fd20bcba9d3
yan_cy <yan_cy@Ctrip.com>
2019-05-04 19:47:19 +0800

    init server

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+
+const app = express();
+
+const config = {};
+
+try {
+    Object.assign(config, require(path.resolve('./package.json')).config);
+} catch (e) {
+    console.log('[node-soa-service] load package.json config fail!')
+}
+
+const port = config.port || process.env.PORT || 8080;
+const vd = (config.vd || '/').replace(/\/+$/g, '');
+
+// 请求体转换为JSON
+app.use(bodyParser.json({strict: true}));
+
+// 将Cookie转换为JSON
+app.use(cookieParser());
+
+app.listen(port, function() {
+    const message = `load config with: ${JSON.stringify(config)}, mount route at ${(vd || '/')}, start at port ${port}`
+    console.log(message)
+});
