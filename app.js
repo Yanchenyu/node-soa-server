96e6634721896fac00414f8b71f1c23ed03051c7
yan_cy <yan_cy@Ctrip.com>
2019-05-06 10:57:37 +0800

    中间件

--- app.js
+++ app.js
@@ -22,6 +22,11 @@ app.use(bodyParser.json({strict: true}));
 // 将Cookie转换为JSON
 app.use(cookieParser());
 
+app.get('/yan/aa', function(req, res, next) {
+    // res.status(200).json({user: 'ids'})
+    res.status(200).send('<h1>123456</h1>')
+});
+
 app.listen(port, function() {
     const message = `load config with: ${JSON.stringify(config)}, mount route at ${(vd || '/')}, start at port ${port}`
     console.log(message)
